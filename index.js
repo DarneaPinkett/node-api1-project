84032c6ce452b95e469fc8f26adf2eb1c77cfa15
DarneaPinkett <56055954+DarneaPinkett@users.noreply.github.com>
2020-04-10 19:02:53 -0400

    database and get

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ server.use(express.json());
 server.get('/', (req, res) => {
     res.send('Hello You!')
 });
+
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then(user => {
+        res.status(200).json({user})
+    })
+    .catch(err => {
+        res.status(500).json({success:false, err});
+    });
+})
